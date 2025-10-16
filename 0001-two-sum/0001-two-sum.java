55f525a786743424e1cc71a35c34da4ea2970abb
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-10-16 20:22:45 +0530

    Time: 0 ms (100%), Space: 44.8 MB (80.72%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+          int[] result=new int[2];
+        for(int i=1;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]+nums[j-i]==target){
+                    result[0]=j-i;
+                    result[1]=j;
+                    return result;
+                }
+            }
+
+        }
+        return result;
+    }
+}
