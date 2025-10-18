ed999dc101e39d09dc4844ed6930cdba0e269cbe
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-10-18 13:30:33 +0530

    Time: 0 ms (100%), Space: 40.9 MB (60.89%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        if(dividend==max && divisor==-1)
+        {
+            return min;
+        }
+        return dividend/divisor;
+        
+        
+    }
+}
