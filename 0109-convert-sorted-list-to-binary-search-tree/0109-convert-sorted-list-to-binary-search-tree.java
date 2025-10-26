62a51f279a574a8c3c66bff7c373a2d0d226c1cb
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-10-26 19:07:56 +0530

    Time: 0 ms (100%), Space: 44.7 MB (89.94%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,38 @@
+class Solution {
+    private ListNode head;
+
+    private int findSize(ListNode head) {
+        ListNode ptr = head;
+        int c = 0;
+        while (ptr != null) {
+            ptr = ptr.next;  
+            c += 1;
+        }
+        return c;
+    }
+
+    private TreeNode convertListToBST(int l, int r) {
+        if (l > r) {
+            return null;
+        }
+
+        int mid = (l + r) / 2;
+
+        TreeNode left = this.convertListToBST(l, mid - 1);
+
+        TreeNode node = new TreeNode(this.head.val);
+        node.left = left;
+
+        this.head = this.head.next;
+
+        node.right = this.convertListToBST(mid + 1, r);
+        return node;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        int size = this.findSize(head);
+        this.head = head;
+
+        return convertListToBST(0, size - 1);
+    }
+}
