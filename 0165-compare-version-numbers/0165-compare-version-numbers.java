657971590185495d0e43a260a44974bc456d12e2
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-01 20:20:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (11.29%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int i = 0, j = 0;
+        int n = version1.length(), m = version2.length();
+
+        while (i < n || j < m) {
+            long num1 = 0, num2 = 0; // long to avoid overflow
+
+            while (i < n && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+
+            while (j < m && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+
+            // skip dots
+            if (i < n && version1.charAt(i) == '.') i++;
+            if (j < m && version2.charAt(j) == '.') j++;
+        }
+
+        return 0;
+    }
+}
