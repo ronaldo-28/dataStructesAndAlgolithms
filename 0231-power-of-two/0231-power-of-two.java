3b98018b8dd3eb2d42baf13e2b2a29ac9d027e00
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-09 11:27:10 +0530

    Time: 0 ms (100%), Space: 41.2 MB (35.36%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        // Use bitwise AND to check if only one bit is set
+        return (n & (n - 1)) == 0;
+    }
+}
