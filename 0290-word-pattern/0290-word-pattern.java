ca580d8ea66a31e61c5e63fc3723959bd0aa8448
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-14 18:34:18 +0530

    Time: 0 ms (100%), Space: 42.9 MB (5.49%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+        String words[] = s.split(" ");
+
+        if(pattern.length() != words.length){
+            return false;
+        }        
+       
+        for(int i=0; i<pattern.length(); i++){
+
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(words[i])){
+                    return false;
+                }
+            } else{
+                if(map.containsValue(words[i])){
+                    return false;
+                }
+                map.put(ch, words[i]);
+            }
+
+        }
+        return true;
+    }
+}
