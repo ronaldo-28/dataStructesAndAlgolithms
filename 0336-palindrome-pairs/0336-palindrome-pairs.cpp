f5c0c6d0ea76a97581f002cb9b7bc973be29d5dd
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-19 22:21:20 +0530

    Time: 1793 ms (51.31%), Space: 576.3 MB (69.64%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.cpp
+++ 0336-palindrome-pairs/0336-palindrome-pairs.cpp
@@ -1,38 +1,67 @@
 class Solution {
 public:
-    vector<vector<int>> palindromePairs(const vector<string> &w) {
-        using ull = unsigned long long;
-        vector<vector<int>> r;
-        unordered_map<ull, int> m;
-        constexpr ull p = 31, l = 301;
-        ull fh[l]{0}, bh[l]{0}, pp[l]{1};
-        bool ss[l]{};
-        for (int i = 1; i < l; ++i) pp[i] = p * pp[i - 1];
-        for (int i = 0; i < w.size(); ++i) {
-            ull h = 0; for (int c : w[i]) h = p * h + c;
-            m.emplace(h, i);
-            ss[w[i].size()] = true;
+    bool pal(int s, int e, string st){
+        while(s < e){
+            if(st[s] != st[e]) return false;
+            s++;
+            e--;
         }
-        for (int i = 0; i < w.size(); ++i) {
-            const auto &s = w[i];
-            const int n = s.size();
-            for (int j = 0; j < n; ++j) {
-                fh[j + 1] = p * fh[j] + s[j];
-                bh[j + 1] = p * bh[j] + s[n - 1 - j];
-            }
-            for (int j = 0, k = n; k >= 0; ++j, --k) if (ss[j]) {
-                if (fh[n] - pp[k] * fh[j] == bh[k]) {
-                    auto a = m.find(bh[n] - pp[j] * bh[k]);
-                    if (a != end(m) && (k != 0 ? i != a->second : i < a->second))
-                        r.push_back({ i, a->second });
-                }
-                if (bh[n] - pp[k] * bh[j] == fh[k]) {
-                    auto a = m.find(bh[j]);
-                    if (a != end(m) && (k != 0 ? i != a->second : i < a->second))
-                        r.push_back({ a->second, i });
-                }
-            }
+        return true;
+    }
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        set<vector<int>> v;
+        unordered_map<string,int>mp;
+        int n = words.size();
+        for(int i = 0; i < n; i++){
+            mp[words[i]] = i;
         }
-        return r;
+        for(int i = 0; i < n; i++){
+            string temp = "";
+            int n1 = words[i].size();
+            if(mp.find(temp) != mp.end() && mp[temp] != i){
+                if(pal(0,n1-1,words[i])){
+                    vector<int>t;
+                    t.push_back(i);
+                    t.push_back(mp[temp]);
+                    v.insert(t);
+                    v.insert({mp[temp],i});
+                }              
+            }
+            for(int j = 0; j < n1; j++){
+                temp = words[i][j] + temp;
+                if(mp.find(temp) != mp.end() && mp[temp] != i){
+                    if(pal(j+1,n1-1,words[i])){
+                        vector<int>t;
+                        t.push_back(i);
+                        t.push_back(mp[temp]);
+                        v.insert(t);
+                    }
+                }
+            }
+            for(int j = 0; j < 26; j++){
+                char c = 'a' + j;
+                string temp1 = c + temp;
+                if(mp.find(temp1) != mp.end() && mp[temp1] != i){
+                    vector<int>t;
+                    t.push_back(i);
+                    t.push_back(mp[temp1]);
+                    v.insert(t);
+                }
+            }
+            temp = "";
+            for(int j = n1-1; j >= 0; j--){
+                temp += words[i][j];
+                if(mp.find(temp) != mp.end() && mp[temp] != i){
+                    if(pal(0,j-1,words[i])){
+                        vector<int>t;
+                        t.push_back(mp[temp]);
+                        t.push_back(i);
+                        v.insert(t);
+                    }
+                }
+            }            
+        }
+        vector<vector<int>>res(v.begin(),v.end());
+        return res;
     }
 };
